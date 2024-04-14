87ed944bb3cbc526394ac722fe61a725de7257bc
Fredrik Falkman <Ff@ffd.se>
2024-04-14 17:25:24 +0200

    Created LUA-file

---
+++ Navlights_on_armed.lua
@@ -0,0 +1,9 @@
+--[[
+ Script to use LED strips as position lights.
+ For this script we will use two strips with up to 8 LEDs each.
+--]]
+local num_leds = 8
+local timer = 0
+
+-- Brightness for green or red light.
+local br_color = 255
