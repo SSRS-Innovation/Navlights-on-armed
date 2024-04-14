5e78349b27b1738969e5cf547136a7b2c68a0a87
Fredrik Falkman <Ff@ffd.se>
2024-04-14 22:32:54 +0200

    Working script

--- Navlights_on_armed.lua
+++ Navlights_on_armed.lua
@@ -2,19 +2,18 @@
  LUA-script for Ardupilot to turn on/off e.g. navlights on a servo channel based on arming state
 --]]
 
-local navlight_servo_channel = 8
+local navlight_servo_channel = 7
 local navlight_pwm_off = 1000
 local navlight_pwm_on = 2000
 
 function navlights()
   if arming:is_armed() then
-    SRV_Channels:set_output_pwm_chan(7, 2000)
+    SRV_Channels:set_output_pwm_chan((navlight_servo_channel), (navlight_pwm_on))
     gcs:send_text(6, "Nav Lights on")
   else
-    SRV_Channels:set_output_pwm_chan(7, 1000)
+    SRV_Channels:set_output_pwm_chan((navlight_servo_channel), (navlight_pwm_off))
     gcs:send_text(6, "Nav Lights off")
   end
   return navlights, 1000
 end
-
 return navlights, 1000
