fe0410b5be652373f91b9adec19e42928089f5f4
Fredrik Falkman <Ff@ffd.se>
2024-04-14 18:12:57 +0200

    First complete script - not tested

--- Navlights_on_armed.lua
+++ Navlights_on_armed.lua
@@ -1,9 +1,19 @@
 --[[
- Script to use LED strips as position lights.
- For this script we will use two strips with up to 8 LEDs each.
+ LUA-script for Ardupilot to turn on/off e.g. navlights on a servo channel based on arming state
 --]]
-local num_leds = 8
-local timer = 0
 
--- Brightness for green or red light.
-local br_color = 255
+local navlight_servo_channel = 8
+local navlight_pwm_off = 1000
+local navlight_pwm_on = 2000
+
+function navlights()
+  if arming:is_armed() then
+    SRV_Channels:set_output_pwm_chan((navlight_servo_channel), (navlight_pwm_on))
+    gcs:send_text(6, "Nav Lights on")
+  else
+    SRV_Channels:set_output_pwm_chan((navlight_servo_channel), (navlight_pwm_off))
+    gcs:send_text(6, "Nav Lights off")
+  end
+end
+
+return navlights(), 1000
