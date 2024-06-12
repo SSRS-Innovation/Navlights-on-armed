e5c3cec5c8615c83cbe5b5fe2c74c77baac4b7a4
Fredrik Falkman <Ff@ffd.se>
2024-06-12 15:05:02 +0200

    Commented out gcs:send_text

--- Navlights_on_armed.lua
+++ Navlights_on_armed.lua
@@ -9,10 +9,10 @@ local navlight_pwm_on = 2000
 function navlights()
   if arming:is_armed() then
     SRV_Channels:set_output_pwm_chan((navlight_servo_channel), (navlight_pwm_on))
-    gcs:send_text(6, "Nav Lights on")
+    -- gcs:send_text(6, "Nav Lights on")
   else
     SRV_Channels:set_output_pwm_chan((navlight_servo_channel), (navlight_pwm_off))
-    gcs:send_text(6, "Nav Lights off")
+    -- gcs:send_text(6, "Nav Lights off")
   end
   return navlights, 1000
 end
